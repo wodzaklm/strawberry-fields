f29925022a4d7eb34c30032fe02533342199ac6e
nathanhayas <nathanhayas15@gmail.com>
2026-05-06 09:32:16 -0400

    Add files via upload
    This is the code that was used to web scrape the Spotify streaming data from Kworb. This example has the URL for one artist. This code was repeated for each artist using different URLs.

---
+++ Code to Webscrape Kworb Spotify Data.R
@@ -0,0 +1,27 @@
+
+library(rvest)
+library(dplyr)
+library(stringr)
+
+# Testing if we are allowed to web scrape
+robotstxt::paths_allowed(domain = 
+"https://kworb.net/spotify/artist/3WrFJ7ztbogyGnTHbHJFl2_songs.html")
+## Returned true so we are allowed to scrape
+
+# URL
+url <- "https://kworb.net/spotify/artist/3WrFJ7ztbogyGnTHbHJFl2_songs.html"
+
+# Read page
+page <- read_html(url)
+
+# Extract all tables (the first one is usually the songs table)
+tables <- page %>% html_table(fill = TRUE)
+
+# Inspect how many tables
+length(tables)
+
+# Select the correct table
+df <- tables[[2]]
+
+# Copy df to clipboard
+write.table(df, "clipboard", sep = "\t", row.names = FALSE)
